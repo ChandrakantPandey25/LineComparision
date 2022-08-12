064177f3ab543c3197030d69900aa665a3e077fd
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 00:12:58 -0700

    Added line length

---
+++ LineComparision/LineComparision/LineLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    class LineLength
+    {
+        public  double Line_Length()
+        {
+            Console.WriteLine("Enter the value of first coordinate x1 and y1");
+            Double x1 = Convert.ToDouble(Console.ReadLine());
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of second coordinate x2 and y2");
+            Double x2 = Convert.ToDouble(Console.ReadLine());
+            Double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Double num1 = Math.Pow((x2 - x1), 2);
+            Double num2 = Math.Pow((y2 - y1), 2);
+            Double line = Math.Sqrt(num1 + num2);
+            return line;
+
+
+
+
+        }
+    }
+}
---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision");
+            Console.WriteLine("Enter 1 for Line Length");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    LineLength line = new LineLength();
+                    double lineOne=line.Line_Length();
+                    Console.WriteLine("Length of the Line is :"+lineOne);
+                    break;
+            }
+
+        }
+    }
+}
